0f994e93e68b61752b1ba6bc1ea92e4f7e80c994
eljones7 <eljones7@crimson.ua.edu>
2022-03-03 11:25:58 -0600

    Update to writing excel file

--- Scripts/WriteToDB.py
+++ Scripts/WriteToDB.py
@@ -1,12 +1,33 @@
 # Python script that parses data sent by middle end script and writes it to the EL database
+import openpyxl
 
 # Constants (may move to a seperate constants file later for organization, doesn't really matter now)
 ELDB_FILE_PATH = "../Data/Example_DB.xlsx"
 
-# imports
+# Return the max number of rows that have data so we can append to the end
+def __getMaxRows(sheet):
+    rows = 0
+    for max_row, row in enumerate(sheet, 1):
+        if not all(col.value is None for col in row):
+            rows += 1
+    return rows
 
-# Temporary fake data, will change later
-# Store as a list of lists where each sublist is in form: [field, value]
-exampleInput = [["Name", "Biology Outreach"], ["College", "Arts & Science"], ["Department", "Biology"],
-                ["Academic Level", "Undergraduate"]]
-                
+# 'Main Method' of script
+def writeToExcel():
+    # Temporary fake data, will change later
+    exampleInput = [["Name", "Biology Outreach"], 
+                    ["College", "Arts & Science"], 
+                    ["Department", "Biology"],
+                    ["Academic Level", "Undergraduate"]]
+
+    # Load the db and get the sheet the projects are in
+    db = openpyxl.load_workbook(ELDB_FILE_PATH)
+    projectSheet = db['Sheet1']
+
+    newRow = __getMaxRows(projectSheet) + 1
+    print(newRow)
+   # for i in exampleInput:
+
+
+# Driver Code (move to different file later)
+writeToExcel()
