2e8f5f1c7e68f2336fb88e338fb251bdbc7bdd68
jdwaid <jdwaid@crimson.ua.edu>
2022-03-31 14:56:47 -0500

    Added a javascript folder. Added a javascript that reads form fields directly upon submission and displays to browser console

---
+++ Scripts/javascripts/form_testing.js
@@ -0,0 +1,63 @@
+$form.submit(function(e) {
+    
+    var projectName = $form.find("[name='project_name']").val(); // variable from form
+    var college = $form.find("[name='college']").val(); // variable from form
+    var department = $form.find("[name='department']").val(); // variable from form
+    var classLevel = $form.find("[name='class_level']").val(); // variable from form
+    var primaryFocusArea = $form.find("[name='primary_focus_area']").val(); // variable from form
+    var participantType = $form.find("[name='participant_type']").val(); // variable from form
+    var numberOfParticipants = $form.find("[name='number_of_participants']").val(); // variable from form
+    var startDate = $form.find("[name='start_date']").val(); // variable from form
+    var endData = $form.find("[name='end_date']").val(); // variable from form
+    var semester = $form.find("[name='semester']").val(); // variable from form
+    var year = $form.find("[name='year']").val(); // variable from form
+    var status = $form.find("[name='status']").val(); // variable from form
+    var compensation = $form.find("[name='compensation']").val(); // variable from form
+    var compensation1 = $form.find("[name='compensation_1']").val(); // variable from form
+    var opportunityType = $form.find("[name='opportunity_type']").val(); // variable from form
+    var universityPartner = $form.find("[name='university_partner']").val(); // variable from form
+    var workingWithCommunityPartner = $form.find("[name='working_with_community_partner']").val(); // variable from form
+    var association = $form.find("[name='association']").val(); // variable from form
+    var type = $form.find("[name='type']").val(); // variable from form
+    var contact = $form.find("[name='contact']").val(); // variable from form
+    var website = $form.find("[name='website']").val(); // variable from form
+    var location = $form.find("[name='location']").val(); // variable from form
+    var financing = $form.find("[name='financing']").val(); // variable from form
+    var costToStudent = $form.find("[name='cost_to_student']").val(); // variable from form
+    var costToUaPartner = $form.find("[name='cost_to_ua_partner']").val(); // variable from form
+    var costToCommunityPartner = $form.find("[name='cost_to_community_partner']").val(); // variable from form
+    var timeCommitment = $form.find("[name='time_commitment']").val(); // variable from form
+    var admissionType = $form.find("[name='admission_type']").val(); // variable from form
+    var impact = $form.find("[name='impact']").val(); // variable from form
+
+    console.log(projectName); //Check if it works
+    console.log(college); //Check if it works
+    console.log(department); //Check if it works
+    console.log(classLevel); //Check if it works
+    console.log(primaryFocusArea); //Check if it works
+    console.log(participantType); //Check if it works
+    console.log(numberOfParticipants); //Check if it works
+    console.log(startDate); //Check if it works
+    console.log(endData); //Check if it works
+    console.log(semester); //Check if it works
+    console.log(year); //Check if it works
+    console.log(status); //Check if it works
+    console.log(compensation); //Check if it works
+    console.log(compensation1); //Check if it works
+    console.log(opportunityType); //Check if it works
+    console.log(universityPartner); //Check if it works
+    console.log(workingWithCommunityPartner); //Check if it works
+    console.log(association); //Check if it works
+    console.log(type); //Check if it works
+    console.log(contact); //Check if it works
+    console.log(website); //Check if it works
+    console.log(location); //Check if it works
+    console.log(financing); //Check if it works
+    console.log(costToStudent); //Check if it works
+    console.log(costToUaPartner); //Check if it works
+    console.log(costToCommunityPartner); //Check if it works
+    console.log(timeCommitment); //Check if it works
+    console.log(admissionType); //Check if it works
+    console.log(impact); //Check if it works
+
+});
