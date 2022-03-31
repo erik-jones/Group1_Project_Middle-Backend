28bc8eed8e4194483eb2356d26252ebcdaabddac
julies99 <99441901+julies99@users.noreply.github.com>
2022-03-31 13:11:41 -0500

    Edit comments in EditProject.py

--- Scripts/EditProject.py
+++ Scripts/EditProject.py
@@ -12,19 +12,18 @@ sheet_obj = wb_obj.active
 max_row = sheet_obj.max_row
 max_col = sheet_obj.max_column
 
+# field is the column that needs to change, value is the new value you want to set it to
 def editProject(projectName, field, value):
-
+    
     # find the project within the database (find the row that's needed)
     for i in range(2, max_row + 1):
-        # field is the column that needs to change, value is the new value you want to set it to
         row_obj = sheet_obj.cell(row = i, column = 1)
-        
         if row_obj.value == projectName:
             # identify which column needs to change
             for j in range(1, max_col + 1):
                 cell_obj = sheet_obj.cell(row = 1, column = j)
                 if cell_obj.value == field:
-                    # using the row and column found in previous steps, change the field to the new value
+                    # using the row (i) and column (j) found in previous steps, change the field to the new value
                     sheet_obj.cell(row = i, column = j).value = value
                     
     #overwrite existing database file (save)
