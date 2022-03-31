0006de20fdcb81745398bbe9e16b10706b473d26
julies99 <99441901+julies99@users.noreply.github.com>
2022-03-31 13:08:57 -0500

    Update EditProject.py
    updated editProject function

--- Scripts/EditProject.py
+++ Scripts/EditProject.py
@@ -1,14 +1,31 @@
-# Edits a currently existing project in the excel database
 
-#TODO: 
-# Step 1: find the project within the database (find the row that's needed)
-
-# Step 2: identify which column needs to change
-
-# Step 3: using the row and column found in previous steps, change the field to the new value, save the excel file
+import openpyxl
 
 # Constants
 ELDB_FILE_PATH = "../Data/Example_DB.xlsx"
 
+#to open the workbook
+wb_obj = openpyxl.load_workbook(ELDB_FILE_PATH)
+
+# get workbook active sheet object from the active attribute
+sheet_obj = wb_obj.active
+max_row = sheet_obj.max_row
+max_col = sheet_obj.max_column
+
 def editProject(projectName, field, value):
-    # field is the column that needs to change, value is the new value you want to set it to
+
+    # find the project within the database (find the row that's needed)
+    for i in range(2, max_row + 1):
+        # field is the column that needs to change, value is the new value you want to set it to
+        row_obj = sheet_obj.cell(row = i, column = 1)
+        
+        if row_obj.value == projectName:
+            # identify which column needs to change
+            for j in range(1, max_col + 1):
+                cell_obj = sheet_obj.cell(row = 1, column = j)
+                if cell_obj.value == field:
+                    # using the row and column found in previous steps, change the field to the new value
+                    sheet_obj.cell(row = i, column = j).value = value
+                    
+    #overwrite existing database file (save)
+    wb_obj.save("../Data/Example_DB.xlsx")
