1d714c1eb286a72c3d95fe363152c9e187b16140
jdwaid <jdwaid@crimson.ua.edu>
2022-04-19 14:44:42 -0500

    Updated js code ofr test_console

--- Scripts/javascripts/test_console.js
+++ Scripts/javascripts/test_console.js
@@ -1,4 +1,6 @@
-
-document.getElementById("Button1").onclick = function(){
-    console.log("You successfully called a backend script")
-};
+window.onload = function init()
+{
+    document.getElementById("Button1").onclick = function(){
+        console.log("You successfully called a backend script");
+    };
+}
