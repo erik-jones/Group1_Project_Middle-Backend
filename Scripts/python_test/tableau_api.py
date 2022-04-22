fd737756d68df145c70859a5578ffb46f9a5d4b3
jdwaid <jdwaid@crimson.ua.edu>
2022-04-22 07:28:48 -0500

    Still testing tableau online connections... found some interesting but not too promising results. Need to connect to a datasource

--- Scripts/python_test/tableau_api.py
+++ Scripts/python_test/tableau_api.py
@@ -1,10 +1,35 @@
+import pandas as pd
+
 from tableau_api_lib import TableauServerConnection
 from tableau_api_lib.utils.querying import get_views_dataframe, get_view_data_dataframe
 from tableau_api_lib.utils import flatten_dict_column
+from tableau_api_lib.utils.querying import get_sites_dataframe
+
+SITE_NAMES = None
 
 #Personal Access Token
 #Token Name: test_token
 #Token Secret: C8uI/8q1RaqsDIZTQFDz6Q==:kkgMqOEcyguTCMuyIrX0EspnG5jNhLhm
+
+DB_SERVER_QUERY = """
+{
+    databaseServers {
+        name
+        hostName
+        port
+        connectionType
+        isEmbedded
+    }
+}"""
+
+DATA_CONNECTIONS_QUERY = """
+{
+    databases {
+        name
+        __typename
+        connectionType
+    }
+}"""
 
 tableau_server_config = {
         'my_env': {
@@ -21,8 +46,23 @@ tableau_server_config = {
 conn = TableauServerConnection(tableau_server_config, env='my_env')
 conn.sign_in()
 
-views_df = get_views_dataframe(conn)
-print(views_df.head())
+sites_df = get_views_dataframe(conn)
 
-views_df = flatten_dict_column(views_df, keys=["name","id"], col_name="workbook")
-print(views_df)
+
+db_servers_df = pd.DataFrame()
+data_connections_df = pd.DataFrame()
+
+for index, site in sites_df.iterrows():
+    conn.switch_site(site['contentUrl'])
+    response_a = conn.metadata_graphql_query(DB_SERVER_QUERY)
+    response_b = conn.metadata_graphql_query(DATA_CONNECTIONS_QUERY)
+    site_db_servers_df = pd.DataFrame(response_a.json()['data']['databaseServers'])
+    site_data_connections_df = pd.DataFrame(response_b.json()['data']['databases'])
+    site_db_servers_df['site_name'] = site['name']
+    site_data_connections_df['site_name'] = site['name']
+    db_servers_df = db_servers_df.append(site_db_servers_df, sort=False, ignore_index=True)
+    data_connections_df = data_connections_df.append(site_data_connections_df, sort=False, ignore_index=True)
+    
+combined_db_df = data_connections_df.merge(db_servers_df, how='left', on=['name', 'connectionType', 'site_name'])
+combined_db_df_unique = combined_db_df.drop(columns=['name', 'site_name']).drop_duplicates()
+print(views_df.to_string())
