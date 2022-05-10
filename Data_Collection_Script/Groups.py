2d1da7ee975ccdfe9e26f5e76f417911fee4639b
erik-jones <55299929+erik-jones@users.noreply.github.com>
2022-05-10 14:57:59 -0500

    Update Groups.py

--- Data_Collection_Script/Groups.py
+++ Data_Collection_Script/Groups.py
@@ -29,14 +29,14 @@ def getGroups(token, fields = None):
     
     # Get the total from the first call, then make successive calls until you reach the total
     total = int(data.get('total'))
-    # while len(groupList) < total:
-    #     counter += 1
-    #     print(f"Call to groups: {counter}")
-    #     offset += 50
-    #     params.update({"offset": offset})
+    while len(groupList) < total:
+        counter += 1
+        print(f"Call to groups: {counter}")
+        offset += 50
+        params.update({"offset": offset})
 
-    #     response = requests.get("https://api2.givepulse.com/groups", headers=headers, params=params)
-    #     data = response.json()
-    #     groupList.extend(data.get("results"))
+        response = requests.get("https://api2.givepulse.com/groups", headers=headers, params=params)
+        data = response.json()
+        groupList.extend(data.get("results"))
 
     return groupList
